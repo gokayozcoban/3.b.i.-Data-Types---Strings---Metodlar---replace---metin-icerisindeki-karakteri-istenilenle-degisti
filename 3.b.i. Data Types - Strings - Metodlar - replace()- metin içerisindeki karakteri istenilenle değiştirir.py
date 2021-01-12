72472edd50a12b99faa300a11a348146a5f282b0
gokayozcoban <43710003+gokayozcoban@users.noreply.github.com>
2021-01-12 21:01:55 +0200

    Add files via upload

---
+++ 3.b.i. Data Types - Strings - Metodlar - replace()- metin içerisindeki karakteri istenilenle değiştirir.py
@@ -0,0 +1,9 @@
+# replace()
+# Kod içerisindeki istenilen karakteri ya da kelimeyi değiştirir:
+metin = """ I want to see Özlem here. If this a destiny, I want to change.
+Is this posible?.........."""
+
+print(metin.replace(".........."," \nSure, why not!"))
+I want to see Özlem here. If this a destiny, I want to change.
+Is this posible? 
+Sure, why not!
